57a1a8990c4bfe903e5b14d0027b2b6e73b7cd62
Michael G <michaelgashaw2009@gmail.com>
2019-12-06 11:38:54 -0500

    reverse linkedlist added

---
+++ leetcode/ReverseLinkedList.java
@@ -0,0 +1,40 @@
+public class ReverseLinkedList {
+    public static void main(String[] args) {
+
+    }
+}
+
+
+class ListNode {
+      int val;
+      ListNode next;
+      ListNode(int x) { val = x; }
+  }
+class SolutionReverseNode {
+    public ListNode reverseList(ListNode head) {
+
+        // This is the sample output.
+        // Input:  1->2->3->4->5->NULL
+        // Output: 5->4->3->2->1->NULL
+
+        ListNode previous = null; //the first element should point to null when reversed back, so it is set to null.
+        ListNode nextNode;
+
+        // Check if the initial head isn't null
+        if(head!=null) {
+            while (head.next != null) { //check the node next to the current one isn't null.
+                nextNode = head.next;  // set the next node before cutting the link.
+                head.next = previous;
+                previous = head;
+                head = nextNode;
+
+
+            }
+            // here the head.next points to null so the head should be the element before it.
+            head.next = previous;
+        }
+
+
+        return head;
+    }
+}
