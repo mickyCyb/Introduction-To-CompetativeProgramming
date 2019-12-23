14b8064f4b5992df57c2493c0cb3c41a8ba697c1
Michael G <michaelgashaw2009@gmail.com>
2019-12-23 10:13:30 -0500

    First Bad number Added

---
+++ leetcode/FirstBadVersion.java
@@ -0,0 +1,49 @@
+/**
+ * FirstBadVersion
+ */
+public class FirstBadVersion {
+
+    public static void main(String[] args) {
+        SolutionFirstBadVersion solutionFirstBadVersion = new SolutionFirstBadVersion();
+        int ans = solutionFirstBadVersion.firstBadVersion(5);
+        System.out.println(ans);
+    }
+}
+class VersionControl {
+    boolean isBadVersion(long currNumber) {
+        if (currNumber >= 1702766719) {
+            return true;
+        }
+        return false;
+    }
+}
+
+class SolutionFirstBadVersion extends VersionControl {
+    public int firstBadVersion(int n) {
+        
+        if(isBadVersion(1)){
+            return 1;
+        }
+
+        int currentNumber = n/2;
+        boolean badNumber;
+
+        while(n - currentNumber > 1){
+
+            badNumber = isBadVersion(currentNumber);
+
+            if(badNumber){
+                n = currentNumber;
+                currentNumber = n / 2;
+            }
+            else{
+                currentNumber = ((n - currentNumber) / 2) + currentNumber ;
+            }
+            
+        }
+        if (isBadVersion(n) && !isBadVersion(currentNumber)) {
+            return n;
+        }
+        return currentNumber;
+    }
+}
