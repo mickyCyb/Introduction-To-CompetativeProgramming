0d2a3b30c677b36ace5cdfb6ce7d7af6778e9755
mickyCyb <michaelgashaw2009@gmail.com>
2020-09-15 23:51:48 +0300

    modularizing files into folders

---
+++ Phase2/Week1/SparseArrays.java
@@ -0,0 +1,74 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+// https://www.hackerrank.com/challenges/sparse-arrays/problem
+
+public class SparseArrays {
+
+    // Complete the matchingStrings function below.
+    static int[] matchingStrings(String[] strings, String[] queries) {
+        HashMap<String,Integer> map = new HashMap<>();
+        for(String str : strings){
+            if(!map.containsKey(str)){
+                map.put(str,1);
+            }
+            else{
+                map.put(str,map.get(str)+1);
+            }
+        }
+        int[] ans = new int[queries.length];
+        for(int i=0; i<queries.length; i++){
+            if(map.containsKey(queries[i])){
+                ans[i] = map.get(queries[i]);
+            }
+        }
+        return ans;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int stringsCount = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        String[] strings = new String[stringsCount];
+
+        for (int i = 0; i < stringsCount; i++) {
+            String stringsItem = scanner.nextLine();
+            strings[i] = stringsItem;
+        }
+
+        int queriesCount = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        String[] queries = new String[queriesCount];
+
+        for (int i = 0; i < queriesCount; i++) {
+            String queriesItem = scanner.nextLine();
+            queries[i] = queriesItem;
+        }
+
+        int[] res = matchingStrings(strings, queries);
+
+        for (int i = 0; i < res.length; i++) {
+            bufferedWriter.write(String.valueOf(res[i]));
+
+            if (i != res.length - 1) {
+                bufferedWriter.write("\n");
+            }
+        }
+
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
