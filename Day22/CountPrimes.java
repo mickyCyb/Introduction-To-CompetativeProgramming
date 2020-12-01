99bb754defc5d7473fb46cad98a9a1381f11cf93
mickyCyb <michaelgashaw2009@gmail.com>
2020-12-01 15:43:22 +0300

    decount primes in day 22 modified

--- Day22/CountPrimes.java
+++ Day22/CountPrimes.java
@@ -11,35 +11,67 @@ public class CountPrimes {
         
     }
 }
+        // slow solution, 653ms
+// class SolutionCountPrimes {
+//     public int countPrimes(int n) {
+        
+//       //  int [] nums = new int[n+1];
+//         int x = (int)Math.sqrt(n);
+//         int count =0;
 
-class SolutionCountPrimes {
+//         if(n== 0 || n==1){
+//             return 0;
+//         }
+//         if(n==2){
+//             return 0;
+//         }   
+//         if(n==3){
+//             return 1;
+//         }     
+
+//         for(int i=4; i< n; i++){
+//             for(int j =2; j<=x; j++){
+//                 if(i %j ==0 && i!=j){
+//                    count++;
+//                   // System.out.println(i);
+//                    break;
+                
+//                 }
+//             }
+//         } 
+
+//         return (n-2) -  count;  // 2 numbers have special conditions and are dealt above(2and 3)
+//     }
+//}
+
+        // optimizd solution
+
+class Solution {
     public int countPrimes(int n) {
         
-      //  int [] nums = new int[n+1];
-        int x = (int)Math.sqrt(n);
-        int count =0;
-
-        if(n== 0 || n==1){
+        boolean[] isPrime = new boolean[n];
+        
+        if(n <= 2){
             return 0;
         }
-        if(n==2){
-            return 0;
-        }   
-        if(n==3){
-            return 1;
-        }     
-
-        for(int i=4; i< n; i++){
-            for(int j =2; j<=x; j++){
-                if(i %j ==0 && i!=j){
-                   count++;
-                  // System.out.println(i);
-                   break;
-                
-                }
+        for(int i=2; i<n; i++){
+            isPrime[i] = true;
+        }
+        int count = 0;
+        for(int i = 2; i * i < n; i++){
+            if(!isPrime[i]){
+                continue;
             }
-        } 
-
-        return (n-2) -  count;  // 2 numbers have special conditions and are dealt above(2and 3)
+            for(int j = i * i; j < n; j += i){
+                isPrime[j] = false;
+            }
+        }
+        for(int i=2;i<n;i++){
+            if(isPrime[i]){
+                count++;
+            }
+        }
+        
+        return count;
     }
 }
