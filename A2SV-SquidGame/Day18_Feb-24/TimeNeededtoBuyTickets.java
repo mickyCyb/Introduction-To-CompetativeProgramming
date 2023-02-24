ea46257b4575723803b65d34548871d240e0fd57
Michael Gashaw <michaelgashaw2009@gmail.com>
2023-02-24 11:44:36 +0300

    feb 24, 1 prob added

---
+++ A2SV-SquidGame/Day18_Feb-24/TimeNeededtoBuyTickets.java
@@ -0,0 +1,18 @@
+
+public class TimeNeededtoBuyTickets {
+    
+}
+class Solution {
+    public int timeRequiredToBuy(int[] tickets, int k) {
+        
+        int res = 0;
+        for(int i = 0; i < tickets.length; i++){
+            if(i <= k){
+                res += Math.min(tickets[k], tickets[i]);
+            }else{
+                res += Math.min(tickets[k] - 1, tickets[i]);
+            }
+        }
+        return res;
+    }
+}
